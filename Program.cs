2a32e50b43b3aec7d4ee0af086053ae429088c31
Balanjaneyulu1234 <147976192+Balanjaneyulu1234@users.noreply.github.com>
2025-09-20 11:33:35 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using StudentWebApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Students}/{action=Index}/{id?}");
+
+app.Run();
