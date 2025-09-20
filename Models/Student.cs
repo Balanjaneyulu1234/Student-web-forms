09efc5bb6f7c4eca032679b65e49c72838626c0d
Balanjaneyulu1234 <147976192+Balanjaneyulu1234@users.noreply.github.com>
2025-09-20 11:43:24 +0530

    Create Student.cs

---
+++ Models/Student.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentWebApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+
+        [Required, Range(1, 100)]
+        public int Age { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, StringLength(50)]
+        public string Course { get; set; }
+    }
+}
