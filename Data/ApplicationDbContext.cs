ed14ae25fd47699f16ade267a2a81793b70feef8
Balanjaneyulu1234 <147976192+Balanjaneyulu1234@users.noreply.github.com>
2025-09-20 11:42:41 +0530

    Create ApplicationDbContext.cs

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using StudentWebApp.Models;
+
+namespace StudentWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
